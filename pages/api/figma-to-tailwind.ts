7141c20b9fecd43450f871a542db5ef99aab418a
“whoiskatrin” <katjareznikova@gmail.com>
2023-04-14 20:55:41 +0100

    refactored

--- pages/api/figma-to-tailwind.ts
+++ pages/api/figma-to-tailwind.ts
@@ -1,36 +1,28 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { getFigmaFile } from "../../services/figma";
+// figma-to-tailwind.ts
 
-async function fetchFigmaDesign(figmaLink: string) {
-  const fileId = figmaLink.split("/file/")[1].split("/")[0];
-
-  const figmaFile = await getFigmaFile(fileId);
-
-  return figmaFile;
-}
-
-function convertToTailwind(figmaDesign: any) {
-  return "Your converted Tailwind CSS code here...";
-}
+import { NextApiRequest, NextApiResponse } from "next";
+import { convertFigmaToTailwind } from "../utils/converter";
+import { FigmaFile } from "..utils/types";
+import fetchFigmaData from "../../utils/fetchFigmaData";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    try {
-      const { figmaLink } = req.body;
-      const designData = await fetchFigmaDesign(figmaLink);
-      const tailwindDesign = convertToTailwind(designData);
+  const { figmaLink } = req.body;
 
-      res.status(200).json({ designData, tailwindDesign });
-    } catch (error) {
-      console.error("Error converting Figma design to Tailwind CSS:", error);
-      res
-        .status(500)
-        .json({ message: "Error converting Figma design to Tailwind CSS" });
-    }
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
+  try {
+    // Fetch Figma file data
+    const designData: FigmaFile = await fetchFigmaData(figmaLink);
+
+    // Convert Figma file to Tailwind
+    const tailwindDesign = await convertFigmaToTailwind(designData);
+
+    res.status(200).json({ designData, tailwindDesign });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing the request" });
   }
 }
