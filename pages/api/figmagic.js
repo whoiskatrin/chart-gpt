f75993d254d4bd7a7c776f5f7bf349bcdf9d4f1a
“whoiskatrin” <katjareznikova@gmail.com>
2023-04-16 10:09:42 +0100

    debug

--- pages/api/figmagic.js
+++ pages/api/figmagic.js
@@ -1,53 +1,86 @@
 import { exec } from "child_process";
 import { promisify } from "util";
 import fs from "fs";
-import path from "path";
+import AWS from "aws-sdk";
 
 const execAsync = promisify(exec);
 
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
 async function generateFigmagicFiles(figmaLink) {
-  // Create output directory if it does not exist
-  const outputDir = path.join(process.cwd(), ".figmagic", "output");
-  if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true });
-  }
+  const outputDir = "/tmp/figmagic-output";
 
   await execAsync(
     `figmagic start --url ${figmaLink} --token ${process.env.FIGMA_TOKEN} --output ${outputDir}`
   );
 
   // Read the contents of each generated file
-  const cssContent = fs.readFileSync(path.join(outputDir, "css.css"), "utf-8");
-  const tokensContent = fs.readFileSync(
-    path.join(outputDir, "tokens.ts"),
-    "utf-8"
-  );
-  const elementsContent = fs.readFileSync(
-    path.join(outputDir, "elements.tsx"),
-    "utf-8"
-  );
-  const graphicsContent = fs.readFileSync(
-    path.join(outputDir, "graphics.tsx"),
-    "utf-8"
-  );
+  const cssContent = fs.readFileSync(`${outputDir}/css.css`, "utf-8");
+  const tokensContent = fs.readFileSync(`${outputDir}/tokens.ts`, "utf-8");
+  const elementsContent = fs.readFileSync(`${outputDir}/elements.tsx`, "utf-8");
+  const graphicsContent = fs.readFileSync(`${outputDir}/graphics.tsx`, "utf-8");
   const storybookContent = fs.readFileSync(
-    path.join(outputDir, "storybook.js"),
+    `${outputDir}/storybook.js`,
     "utf-8"
   );
   const descriptionContent = fs.readFileSync(
-    path.join(outputDir, "description.md"),
+    `${outputDir}/description.md`,
     "utf-8"
   );
 
-  // Return an object with the contents of each file
+  // Upload each file to S3
+  await Promise.all([
+    uploadFileToS3(cssContent, "css.css"),
+    uploadFileToS3(tokensContent, "tokens.ts"),
+    uploadFileToS3(elementsContent, "elements.tsx"),
+    uploadFileToS3(graphicsContent, "graphics.tsx"),
+    uploadFileToS3(storybookContent, "storybook.js"),
+    uploadFileToS3(descriptionContent, "description.md"),
+  ]);
+
+  // Return the URLs for each uploaded file
+  const baseS3Url = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.amazonaws.com`;
   return {
-    css: cssContent,
-    tokens: tokensContent,
-    elements: elementsContent,
-    graphics: graphicsContent,
-    storybook: storybookContent,
-    description: descriptionContent,
+    cssUrl: `${baseS3Url}/css.css`,
+    tokensUrl: `${baseS3Url}/tokens.ts`,
+    elementsUrl: `${baseS3Url}/elements.tsx`,
+    graphicsUrl: `${baseS3Url}/graphics.tsx`,
+    storybookUrl: `${baseS3Url}/storybook.js`,
+    descriptionUrl: `${baseS3Url}/description.md`,
   };
+}
+
+async function uploadFileToS3(fileContent, filename) {
+  return s3
+    .upload({
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: filename,
+      Body: fileContent,
+      ACL: "public-read",
+      ContentType: getContentType(filename),
+    })
+    .promise();
+}
+
+function getContentType(filename) {
+  const extension = filename.split(".").pop().toLowerCase();
+  switch (extension) {
+    case "css":
+      return "text/css";
+    case "ts":
+      return "text/plain";
+    case "tsx":
+      return "text/plain";
+    case "js":
+      return "application/javascript";
+    case "md":
+      return "text/markdown";
+    default:
+      return "application/octet-stream";
+  }
 }
 
 export default async function handler(req, res) {
