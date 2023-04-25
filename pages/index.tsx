1b86542e841223617771b385b924e9a9cdfa5d32
whoiskatrin <katjareznikova@gmail.com>
2023-04-25 23:20:05 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,8 +44,8 @@ const HomePage = () => {
     const chartType = await getChartType(inputValue);
 
     try {
-      const libraryPrompt = `Generate a valid JSON in which each element is an object. Strictly using this FORMAT and naming:
-[{ "name": "a", "value": 12 }] for the following description for Recharts. \n\n${inputValue}\n`;
+       const libraryPrompt = `Generate a valid JSON in which each element is an object. Strictly using this FORMAT and naming:
+[{ "name": "a", "value": 12, "color": "#4285F4" }] for the following description for Recharts.\nFor each object CHOOSE a "color" that is the most recognizable color for that object in your opinion. \n\n${inputValue}\n`;
 
       const chartDataGenerate = await generateChartData(libraryPrompt);
 
