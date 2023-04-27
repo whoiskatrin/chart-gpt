a37a82ad7e716600e64abf6e818241a62408ddc0
merrillcook0 <cookjm0@sewanee.edu>
2023-04-26 19:49:22 -0500

    remove console log from last commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ const HomePage = () => {
 
       const libraryPrompt = `Generate a valid JSON in which each element is an object. Strictly using this FORMAT and naming:
 [{ "name": "a", "value": 12, "color": "#4285F4" }] for Recharts API. Make sure field name always stays named name. Instead of naming value field value in JSON, name it based on user metric. If the user includes a color or a hex code for a color, replace #4285F4 with this user inputted color value. \n Make sure the format uses double quotes and property names are string literals. \n\n${inputValue}\n`;
-      console.log(libraryPrompt);
+      
       const chartDataResponse = await axios.post("/api/parse-graph", {
         prompt: libraryPrompt,
       });
@@ -61,7 +61,6 @@ const HomePage = () => {
 
       try {
         parsedData = JSON.parse(chartDataResponse.data);
-        console.log(parsedData);
       } catch (error) {
         setError(true);
         console.error("Failed to parse chart data:", error);
