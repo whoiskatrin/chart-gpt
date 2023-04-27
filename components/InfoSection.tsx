99065a9df88b21e520f691125532d008e6dcf258
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2023-04-27 21:38:08 +0530

    add background color similar to input container

--- components/InfoSection.tsx
+++ components/InfoSection.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 //need to set max-width: 40rem to max-w-4xl class by changing the class to max-w-2xl
 const InfoSection: React.FC = () => {
 	return (
-		<div className='info-container max-w-xl text-left text-center bg-white dark:bg-gray-800 p-8 rounded-xl shadow-lg mb-8'>
+		<div className='info-container max-w-xl text-left text-center bg-indigo-50 dark:bg-gray-800 p-8 rounded-xl shadow-lg mb-8'>
 			<h2 className='text-left font-semibold text-2xl mb-4 font-inter font-bold sm:text-xl flex items-center bg-gradient-to-r from-sky-400 via-violet-600 to-rose-500 bg-clip-text bg-gradient-to-r from-sky-400 via-violet-600 to-rose-500 bg-clip-text text-transparent'>
 				How to use this tool
 			</h2>
