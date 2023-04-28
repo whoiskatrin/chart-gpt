1b686aaff46f1f10648a9b71b735fb80b6191559
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2023-04-28 23:11:01 +0530

    fix min and max height to text area

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,13 +94,13 @@ const HomePage = () => {
 	};
 
 	return (
-		<div className='flex flex-col px-4 items-center justify-center min-h-screen bg-gradient-to-r from-slate-300 to-indigo-50 overflow-x-hidden'>
+		<div className='flex flex-col px-4 items-center justify-center bg-gradient-to-r from-slate-300 to-indigo-50 overflow-y-hidden'>
 			<Header />
 			<Head>
 				<title>AI tool to convert text to beautiful chart</title>
 			</Head>
-			<div className='flex container flex-col items-center w-full max-w-xl mb-6 gap-6 border-gray-300 bg-indigo-50 dark:text-white dark:bg-black dark:border dark:border-white/20 rounded-2xl p-4 h-52'>
-				<form onSubmit={handleSubmit} className='w-full max-w-xl mb-6'>
+			<div className='flex flex-col items-center w-full max-w-xl mb-6 gap-6 border-gray-300 bg-indigo-50 dark:text-white dark:bg-black dark:border dark:border-white/20 rounded-2xl p-4'>
+				<form onSubmit={handleSubmit} className='w-full max-w-xl mb-1'>
 					<div className='flex flex-col items-center justify-center'>
 						<label
 							htmlFor='textInput'
@@ -114,7 +114,7 @@ const HomePage = () => {
 							id='input'
 							rows={3}
 							placeholder=''
-							className='appearance-none font-inter mt-8 border border-gray-300 dark:border-gray-600 shadow-sm flex flex-col items-center justify-center rounded-lg w-full max-w-md py-2 px-3 bg-custom-gray-bg dark:bg-custom-dark-gray text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline text-left min-h-[46%]'
+							className='appearance-none font-inter mt-8 border border-gray-300 dark:border-gray-600 shadow-sm flex flex-col items-center justify-center rounded-lg w-full max-w-md py-2 px-3 bg-custom-gray-bg dark:bg-custom-dark-gray text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline text-left min-h-[120px] max-h-[200px]'
 							value={inputValue}
 							required
 							autoFocus
@@ -131,7 +131,7 @@ const HomePage = () => {
 
 						<button
 							type='submit'
-							className='cursor-pointer font-inter font-semibold py-2 px-4 mt-10 rounded-full blue-button-w-gradient-border text-white text-shadow-0_0_1px_rgba(0,0,0,0.25) shadow-2xl flex flex-row items-center justify-center mt-3'
+							className='cursor-pointer font-inter font-semibold py-2 px-10 mt-10 rounded-full blue-button-w-gradient-border text-white text-shadow-0_0_1px_rgba(0,0,0,0.25) shadow-2xl flex flex-row items-center justify-center mt-3'
 						>
 							Draw
 						</button>
