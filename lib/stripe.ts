08389460266305073841f99feee56f3ff4bd31b1
“whoiskatrin” <katjareznikova@gmail.com>
2023-05-07 17:18:53 +0100

    fix the variable and linting

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import { loadStripe } from '@stripe/stripe-js';
 
 export const getStripe = async () => {
-  const stripePublicKey = process.env.STRIPE_SECRET_KEY;
+  const stripePublicKey = process.env.STRIPE_SECRET_KEY ?? '';
   const stripe = await loadStripe(stripePublicKey);
   return stripe;
 };
