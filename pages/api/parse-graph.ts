c74795b6871f7883566f9e7f0ab24e70aa8b1dcc
“whoiskatrin” <katjareznikova@gmail.com>
2023-05-07 23:10:20 +0100

    fix for that

--- pages/api/parse-graph.ts
+++ pages/api/parse-graph.ts
@@ -1,37 +1,41 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { useSession } from 'next-auth/react';
+import { supabase } from '../../lib/supabase';
 
-const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+const OPENAI_API_URL = 'https://api.openai.com/v1/chat/completions';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  if (req.method !== "POST") {
-    res.status(405).json({ error: "Method not allowed" });
+  const { data: session } = useSession();
+
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method not allowed' });
     return;
   }
 
   const { prompt } = req.body;
   try {
     const response = await fetch(OPENAI_API_URL, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
-        messages: [{ role: "user", content: prompt }],
+        messages: [{ role: 'user', content: prompt }],
         temperature: 0.5,
         max_tokens: 1000,
         n: 1,
-        model: "gpt-3.5-turbo",
+        model: 'gpt-3.5-turbo',
         frequency_penalty: 0.5,
         presence_penalty: 0.5,
       }),
     });
 
     if (!response.ok) {
-      throw new Error("OpenAI API request failed");
+      throw new Error('OpenAI API request failed');
     }
 
     const data = await response.json();
@@ -40,12 +44,17 @@ export default async function handler(
         ? data.choices[0].message.content.trim()
         : null;
     if (!graphData) {
-      throw new Error("Failed to generate graph data");
+      throw new Error('Failed to generate graph data');
     }
     const stringifiedData = graphData.replace(/'/g, '"');
+    await supabase
+      .from('users')
+      .update({ credits: { decrement: 1 } })
+      .eq('email', session?.user?.email);
+
     res.status(200).json(stringifiedData);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Failed to process the input" });
+    res.status(500).json({ error: 'Failed to process the input' });
   }
 }
