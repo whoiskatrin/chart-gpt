23c74d322bb431961a07ec68e612ad71d77c544e
Makenna Smutz <makennasmutz@gmail.com>
2023-05-11 23:15:57 -0400

    Meta info

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/lasso.ico" />
+        <link rel="icon" href="/chart-gpt-logo.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <title>ChartGPT</title>
+        <meta
+          name="description"
+          content="A tool that converts text into beautiful charts"
+        />
       </Head>
       <link
         href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap"
