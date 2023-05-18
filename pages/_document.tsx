8420ce12dd7f34a545c178bcbcbbdffe7532daaf
Makenna Smutz <makennasmutz@gmail.com>
2023-05-18 09:53:58 -0400

    update meta info

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,12 +7,32 @@ export default function Document() {
         <link rel="icon" href="/chart-gpt-logo.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
+
         <title>ChartGPT</title>
+        <meta property="og:title" content="ChartGPT" />
+        <meta property="og:image" content="/chartgpt-og.png" />
+        <meta property="og:url" content="https://chartgpt.dev" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_GB" />
+        <meta
+          property="og:description"
+          content="A tool that converts text into beautiful charts"
+        />
         <meta
           name="description"
           content="A tool that converts text into beautiful charts"
         />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@whoiskatrin" />
+        <meta name="twitter:title" content="ChartGPT - text to charts" />
+        <meta
+          name="twitter:description"
+          content="A tool that converts text into beautiful charts"
+        />
+        <meta name="twitter:image" content="/chartgpt-og.png" />
       </Head>
+
       <link
         href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap"
         rel="stylesheet"
