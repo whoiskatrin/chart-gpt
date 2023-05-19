d468b3ed306dc195589a35ff1cc031ed3c327e74
Mustafa UZUN <mustafauzun0@gmail.com>
2023-05-19 22:56:39 +0300

    Fix key error

--- components/ChartComponent.tsx
+++ components/ChartComponent.tsx
@@ -1,44 +1,45 @@
 import {
-Card,
-Text,
-Metric,
-Callout
-} from '@tremor/react';
-import React from 'react';
-
-interface ChartProps {
-  data: any;
-}
-
-//TODO: dynamic keys instead of default value
-export const Chart: React.FC<ChartProps> = ({
-  data,
-}) => {
-  const renderChart = () => {
+  Card,
+  Text,
+  Metric,
+  Callout
+  } from '@tremor/react';
+  import React from 'react';
+  
+  interface ChartProps {
+    data: any;
+  }
+  
+  //TODO: dynamic keys instead of default value
+  export const Chart: React.FC<ChartProps> = ({
+    data,
+  }) => {
+    const renderChart = () => {
+      return (
+       data.map((item: any, index: any) => {
+            return (
+              <Card key={index} className="mt-2">
+                <Text>Step {index + 1}</Text>
+                <Metric>{item.mainTitle}</Metric>
+                  {item.subheadings.map((subheading: any, index: any) => {
+                    return (
+                    <Callout
+                      className="mt-4 p-4 pb-2"
+                      color="gray"
+                      title={subheading.trim()}
+                      key={index}
+                    ></Callout>
+                    );
+                  })} 
+              </Card>
+            );
+          })
+      )
+    };
+  
     return (<>
-     {data.map((item: any, index: any) => {
-          return (
-            <Card className="mt-2">
-              <Text>Step {index + 1}</Text>
-              <Metric>{item.mainTitle}</Metric>
-                {item.subheadings.map((subheading: any) => {
-                  return (
-                  <Callout
-                    className="mt-4 p-4 pb-2"
-                    color="gray"
-                    title={subheading.trim()}
-                  ></Callout>
-                  );
-                })} 
-            </Card>
-          );
-        })}
-    </>)
+        {renderChart()}</>
+    );
   };
-
-  return (<>
-      {renderChart()}</>
-  );
-};
-
-export default Chart;
+  
+  export default Chart;  
