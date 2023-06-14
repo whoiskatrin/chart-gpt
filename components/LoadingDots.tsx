72cf24199b443d35175d67a7293986e8f8580210
Makenna Smutz <makennasmutz@gmail.com>
2023-06-14 16:18:37 -0400

    fix loader scrunch on mobile

--- components/LoadingDots.tsx
+++ components/LoadingDots.tsx
@@ -3,7 +3,9 @@ import { Ripples } from '@uiball/loaders';
 const LoadingDots = () => {
   return (
     <div className="flex items-center justify-center mx-auto">
-      <Ripples size={45} speed={2} color={'#3b82f6'} />
+      <span className="shrink-0">
+        <Ripples size={45} speed={2} color="#3b82f6" />
+      </span>
       <span className="ml-2 text-zinc-900 dark:text-zinc-100">
         Hang on, finding our artistic inspiration
       </span>
